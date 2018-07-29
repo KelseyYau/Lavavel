2571cb926cc4253b3e0230fef56e24543299fd4b
邱奎元 <yaukelsey@gmail.com>
2018-07-29 16:50:23 +0800

    Move user model to models  folder

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
-Route::get('sigup','UsersController@create')->name('sigup');
+Route::get('/sigup','UsersController@create')->name('sigup');
